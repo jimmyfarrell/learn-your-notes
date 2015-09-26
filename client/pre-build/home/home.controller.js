439b16113e37c940b368f6ee60880137e37e9049
Katy Herrick <katy.herrick@nyu.edu>
2015-09-26 16:51:55 -0400

    orientation maps to sound on Android

--- client/pre-build/home/home.controller.js
+++ client/pre-build/home/home.controller.js
@@ -1,5 +1,45 @@
+Number.prototype.freq_map = function (in_min, in_max, out_min, out_max) {
+  return (this - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+}
+
+
 app.controller('HomeController', function($scope, $http) {
   
-  $scope.msgFromScope = "...And I'm a message from the HomeController scope, just so you know that I work!";
+  var C4 = 261.63;
+  var C6 = 1046.50;
+  var BETA_MIN = -45;
+  var BETA_MAX = 45;
+
+   //create one of Tone's built-in synthesizers and connect it to the master output
+	var synth = new Tone.SimpleSynth().toMaster();
+	var tone = new Tone();
+
+	window.addEventListener('deviceorientation', function(event) {
+  		//$scope.msgFromScope = "I'm playing " + event.beta;
+  		console.log("Orientation val: " + event.beta);
+  		var orientation = event.beta+0;
+  		if (orientation < BETA_MIN) {
+  			var freq = C4;
+  		} else if (orientation > BETA_MAX) {
+  			var freq = C6;
+  		}
+  		else {
+			var freq = orientation.freq_map(BETA_MIN, BETA_MAX, C4, C6);
+		}
+		var note = tone.frequencyToNote(freq);
+		$scope.msgFromScope = "I'm playing " + note + ".\n" +
+								"My frequency is " + freq + "Hz\n" +
+								"\nbecause the phone's orientation is " + orientation;
+  		$scope.$digest();
+
+  		//Play the note
+		synth.triggerAttack(note);
+	}); 
+	
+	
+
 
 });
+
+// working range is 0 to -90 
+
