4932a24cb216e639bc10a35dc84131787c14c548
jimmyfarrell <jfarrell22@gmail.com>
2015-09-26 19:24:38 -0400

    Finished

--- client/pre-build/home/home.controller.js
+++ client/pre-build/home/home.controller.js
@@ -6,10 +6,14 @@ app.controller('HomeController', function($scope) {
     var socket = io();
 
     $scope.note = 'Learn Your Notes!';
+    $scope.image = 'no';
 
     socket.on('note:change', function(note) {
 		synth.triggerAttack(note);
+        if (note.indexOf('#') > -1) $scope.image = note.replace('#', 's');
+        else $scope.image = note;
         $scope.note = note;
+        $scope.$digest();
     });
 
     socket.on('disconnect:phone', function() {
